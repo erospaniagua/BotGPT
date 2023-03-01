58706c222efdf95e6356d398f3ee284225fba3ac
erospaniagua <eros.gear@gmail.com>
2023-02-28 18:52:52 -0600

    Add files via upload

---
+++ BOTGPT/app.js
@@ -0,0 +1,27 @@
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MockAdapter = require('@bot-whatsapp/database/mock')
+
+const ChatGPTClass = require('./chatgpt.class')
+
+const createBotGTP = async ({ provider, database }) => {
+    return new ChatGPTClass( database, provider );
+};
+
+
+const main = async () => {
+    const adapterDB = new MockAdapter()
+    const adapterProvider = createProvider(BaileysProvider)
+
+    createBotGPT({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    QRPortalWeb()
+}
+
+main()
---
+++ BOTGPT/chatgpt.class.js
@@ -0,0 +1,45 @@
+const { CoreClass } = require("@bot-whatsapp/bot");
+
+class ChatGPTClass extends CoreClass{
+    queue =[];
+    optionsGPT = { model: "text-davinci-003" };
+    openai = undefined;
+    
+    constructor(_database , _provider , _optionsgpt ={}) {
+        super(null, _database, _provider); 
+        this.optionsGPT = {...this.optionsGPT , ..._optionsgpt};
+        this.init().then();
+            
+    }
+
+    init = async() => {
+        const {ChatGPTAPI} = await import("chatgpt");
+        this.openai = new ChatGPTAPI({
+            apiKey: process.env.OPENAI_API_KEY,
+        })
+    };
+
+    handleMsg = async (ctx) => {
+        const { from, body } = ctx;
+
+        const completion = await this.openai.sendMessage(body,{
+            conversationId: !this.queue.length 
+            ? undefined 
+            :this.queue[this.queue.length - 1].conversationId,
+            perentMessageId: !this.queue.length
+            ? undefined 
+            :this.queue[this.queue.length - 1].id,
+        });
+
+        this.queue.push(completion);
+
+        const parseMesage = {
+            ...completion,
+            awnser: completion, text,
+        };
+        this.sendFlowSimple([parseMesage], from)
+    };
+
+}
+
+module.exports = ChatGPTClass
