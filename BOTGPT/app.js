bb4caa1708d2e292b0fa68b9ae86c29270c494c7
erospaniagua <eros.gear@gmail.com>
2023-02-28 20:01:40 -0600

    Update app.js

--- BOTGPT/app.js
+++ BOTGPT/app.js
@@ -1,27 +1,25 @@
-const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot');
 
-const QRPortalWeb = require('@bot-whatsapp/portal')
-const BaileysProvider = require('@bot-whatsapp/provider/baileys')
-const MockAdapter = require('@bot-whatsapp/database/mock')
+const QRPortalWeb = require('@bot-whatsapp/portal');
+const BaileysProvider = require('@bot-whatsapp/provider/baileys');
+const MockAdapter = require('@bot-whatsapp/database/mock');
 
-const ChatGPTClass = require('./chatgpt.class')
+const ChatGPTClass = require('./chatgpt.class');
 
-const createBotGPT = async ({ provider, database }) => {
+const createBotGTP = async ({ provider, database }) => {
     return new ChatGPTClass( database, provider );
 };
 
 
 const main = async () => {
-    const adapterDB = new MockAdapter()
-    const adapterProvider = createProvider(BaileysProvider)
+    const adapterDB = new MockAdapter();
+    const adapterProvider = createProvider(BaileysProvider);
 
-    createBotGPT({
-        flow: adapterFlow,
+    createBotGTP({
         provider: adapterProvider,
         database: adapterDB,
-    })
+    });
 
-    QRPortalWeb()
+    QRPortalWeb();
 }
 
-main()
